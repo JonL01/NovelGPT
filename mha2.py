4163c8a5fbfc198e8e318df92eac1718488b5fa2
Jing <jingl_01@163.com>
2024-04-16 13:16:44 +0800

    mha2

--- mha2.py
+++ mha2.py
@@ -1,9 +1,10 @@
 import torch
 import torch.nn as nn
-from torch.nn import functional as F
+from torch import functional as F
 n_embd = 384
-block_size = 256
 dropout = 0.2
+block_size = 256
+
 class Head(nn.Module):
     def __init__(self, head_size):
         super.__init__()
@@ -13,28 +14,29 @@ class Head(nn.Module):
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
 
         self.dropout = nn.Dropout(dropout)
-
     def forward(self, x):
         B, T, C = x.shape
+
         q = self.query(x)
         k = self.key(x)
         wei = q @ k.transpose(-2, -1) * k.shape[-1]**-0.5
-        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
+        wei = wei.masked_fill(self.tril[:T, :T]==0, float('-inf'))
         wei = F.softmax(wei, dim=-1)
         wei = self.dropout(wei)
-        
+
         v = self.value(x)
         out = wei @ v
         return out
+
 class MultiHeadAttention(nn.Module):
-    def __init__(self, num_heads, head_size):
+    def __init__(self, nums_head, head_size):
         super.__init__()
-        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
-        self.proj = nn.Linear(num_heads * head_size, n_embd)
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(nums_head)])
+        self.proj = nn.Linear(nums_head*head_size, n_embd)
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         out = torch.cat([h(x) for h in self.heads], dim=-1)
         out = self.dropout(self.proj(out))
+
         return out
-        
